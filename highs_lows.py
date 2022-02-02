f1e77afb5936e2368e9362ac5e84fbd1d816564f
Ruslan Nazarov <hlfan2@yandex.ru>
2022-02-02 16:10:23 +0700

    add fill between lines

--- highs_lows.py
+++ highs_lows.py
@@ -17,12 +17,15 @@ with open(weather_file) as weather:
 
 # Нанесение данных на диаграму.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(dates, highs, c='red')
-plt.plot(dates, lows, c='blue')
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+# Закрашевает область между линиями.
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Форматирование диаграмы.
 plt.title('Daily Highs and Lows Temperatures, July 2014', fontsize=24)
 plt.xlabel('', fontsize=16)
+# Поворачивает подписи на оси X.
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
