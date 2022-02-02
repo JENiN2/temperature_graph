cacf17873430c3aaf3e3b38bb4e3be806d5a2e96
Ruslan Nazarov <hlfan2@yandex.ru>
2022-02-02 15:58:33 +0700

    formatted date added

--- highs_lows.py
+++ highs_lows.py
@@ -1,26 +1,27 @@
 import csv
+from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-# Чтение максимальных температур из файла.
+# Чтение дат и максимальных температур из файла.
 weather_file = 'data/sitka_weather_2014.csv'
 with open(weather_file) as weather:
     reader = csv.reader(weather)
     header_row = next(reader)
 
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        dates.append(datetime.strptime(row[0], '%Y-%m-%d'))
         highs.append(int(row[1]))
-
-print(highs)
 
 # Нанесение данных на диаграму.
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 # Форматирование диаграмы.
 plt.title('Daily Highs Temperatures, July 2014', fontsize=24)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
