a950d29e8b82b023666ba7b3953e40c00ebe7528
Ruslan Nazarov <hlfan2@yandex.ru>
2022-02-02 16:02:01 +0700

    add lows temperatures

--- highs_lows.py
+++ highs_lows.py
@@ -3,23 +3,25 @@ from datetime import datetime
 
 from matplotlib import pyplot as plt
 
-# Чтение дат и максимальных температур из файла.
+# Чтение дат, температурных минимумов и максимумов из файла.
 weather_file = 'data/sitka_weather_2014.csv'
 with open(weather_file) as weather:
     reader = csv.reader(weather)
     header_row = next(reader)
 
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
         dates.append(datetime.strptime(row[0], '%Y-%m-%d'))
+        lows.append(int(row[3]))
         highs.append(int(row[1]))
 
 # Нанесение данных на диаграму.
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
 
 # Форматирование диаграмы.
-plt.title('Daily Highs Temperatures, July 2014', fontsize=24)
+plt.title('Daily Highs and Lows Temperatures, July 2014', fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel('Temperature (F)', fontsize=16)
