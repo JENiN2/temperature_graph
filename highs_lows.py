88c31cf40732775e2cb48fe8b892f6330bc7fa3c
Ruslan Nazarov <hlfan2@yandex.ru>
2022-02-02 15:49:08 +0700

    draw figure

--- highs_lows.py
+++ highs_lows.py
@@ -1,5 +1,7 @@
 import csv
 
+from matplotlib import pyplot as plt
+
 # Чтение максимальных температур из файла.
 weather_file = 'data/sitka_weather_2014.csv'
 with open(weather_file) as weather:
@@ -8,6 +10,18 @@ with open(weather_file) as weather:
 
     highs = []
     for row in reader:
-        highs.append(row[1])
+        highs.append(int(row[1]))
 
 print(highs)
+
+# Нанесение данных на диаграму.
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c='red')
+
+# Форматирование диаграмы.
+plt.title('Daily Highs Temperatures, July 2014', fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
