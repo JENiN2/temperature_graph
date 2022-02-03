ca49bfee47ca0cf81e6936863ab60c50f035648e
Ruslan Nazarov <hlfan2@yandex.ru>
2022-02-03 12:23:07 +0700

    add argparser

--- highs_lows.py
+++ highs_lows.py
@@ -1,10 +1,25 @@
 import csv
 from datetime import datetime
+import argparse
 
 from matplotlib import pyplot as plt
 
+# Выбор для какого места отображать погоду.
+parser = argparse.ArgumentParser(description='Creating Daily Highs and Lows Temperatures Diagram')
+parser.add_argument('--place', type=int, choices=[1, 2],
+                    help='Choose: 1 - Sitka, 2 - Death Valley', required=True)
+args = parser.parse_args()
+place = args.place
+
+weather_file = ''
+if place == 1:
+    place = 'Sitka, Alaska'
+    weather_file = 'data/sitka_weather_2014.csv'
+elif place == 2:
+    place = 'Death Valley, California'
+    weather_file = 'data/death_valley_2014.csv'
+
 # Чтение дат, температурных минимумов и максимумов из файла.
-weather_file = 'data/sitka_weather_2014.csv'
 with open(weather_file) as weather:
     reader = csv.reader(weather)
     header_row = next(reader)
@@ -31,7 +46,7 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Форматирование диаграмы.
-plt.title('Daily Highs and Lows Temperatures, July 2014', fontsize=24)
+plt.title(f'Daily Highs and Lows Temperatures, July 2014\n{place}', fontsize=24)
 plt.xlabel('', fontsize=16)
 # Поворачивает подписи на оси X.
 fig.autofmt_xdate()
